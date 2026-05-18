d32a84eb2df0dbbbd76b464f6745ddfa1fd4ddd6
Eliezir <eliezirneto@gmail.com>
2026-05-18 14:13:46 -0300

    chore(frontend): add React Router and related dependencies for improved routing

---
+++ frontend/src/renderer/src/router.tsx
@@ -0,0 +1,33 @@
+import {
+  createRootRoute,
+  createRoute,
+  createRouter,
+  Outlet
+} from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import App from './App'
+
+const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      <Outlet />
+      {import.meta.env.DEV && <TanStackRouterDevtools position="bottom-right" />}
+    </>
+  )
+})
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: App
+})
+
+const routeTree = rootRoute.addChildren([indexRoute])
+
+export const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
