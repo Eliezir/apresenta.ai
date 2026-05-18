5e39f527ff82f9e2358f602be8c4d7c06a2c1d15
Eduardo Pereira <doardoperera@gmail.com>
2026-05-17 22:10:56 -0300

    feat: inicialização do projeto spring

---
+++ src/test/java/com/dev/apresenta_ia/ApresentaIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.apresenta_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApresentaIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
